a0c323c22ec4f88e2db59142efca27df2f102b05
fulya_ture <fulyature@gmail.com>
2023-07-06 19:58:26 +0300

    handleevents

--- src/1-events/Event.jsx
+++ src/1-events/Event.jsx
@@ -1,13 +1,22 @@
 const Event = () => {
+  let message = "HELLO";
   const handleClick = (e) => {
     alert("Hi");
     console.log(e);
     console.log(e.target);
   };
-
+  const handleChange = (e) => {
+    console.log(e.target.name);
+    console.log(e.target);
+    message = "MERHABA";
+  };
   return (
     <div>
+      <h1>{message}</h1>
       <button onClick={handleClick}>Click</button>
+      <button onClick={() => alert("hi alion")}>Hi</button>
+
+      <button onClick={handleChange}>Change</button>
     </div>
   );
 };
